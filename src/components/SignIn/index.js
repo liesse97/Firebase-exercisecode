0e4b9257d429b1d89dfaa621e9ae56d528c155b4
liesse97 <ishimwelieish@gmail.com>
2020-02-10 12:02:34 +0100

    initial commit

---
+++ src/components/SignIn/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const Admin = () => (
+    <div>
+        <h1>Sign in</h1>
+    </div>
+);
+export default Admin;
---
+++ src/components/SignOut/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const Admin = () => (
+    <div>
+        <h1>Sign up</h1>
+    </div>
+);
+export default Admin;
